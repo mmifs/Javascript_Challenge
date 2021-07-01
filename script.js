4f828f10b021f0a60ea8fd72b193726d1b73e127
mmifs <85654339+mmifs@users.noreply.github.com>
2021-06-30 23:24:21 -0400

    pushLowerCase function working

--- script.js
+++ script.js
@@ -6,23 +6,27 @@ var generateBtn = document.querySelector("#generate");
 // 1: code to make button trigger on click
 // 2: create prompts upon clicking button to ask parameters of password, push parameters into an array
 // 3: create functions which utilizes parameters to create password
-//function pushLowerCase() {
-  //for (var i = 0; i < lowerCase.length; i++){
-    //possiblePassword.push(lowerCase[i])
- // }
+function pushLowerCase() {
+  for (var i = 0; i < lowerCase.length; i++){
+    possiblePassword.push(lowerCase[i])
+  }
+}
+
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var special = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "{", "[", "}", "]", ";", ":", "<", ">", "?"];
+var possiblePassword = []
+
 
 function writePassword() {
   window.alert("Starting password generation...");
+
+  var isLowerCase = window.confirm('Do you want lowercase letters in your password?');
+  if (isLowerCase) {
+    pushLowerCase()
+  }
 }
-  //var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  //var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  //var numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-  //var special = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "{", "[", "}", "]", ";", ":", "<", ">", "?"];
-  //var possiblePassword = []
-  //var isLowerCase = window.confirm('Do you want lowercase letters in your password?');
-  //if (isLowerCase) {
-    //pushLowerCase()
-  //}
   //var password = generatePassword();
   //var passwordText = document.querySelector("#password");
 
@@ -31,4 +35,4 @@ function writePassword() {
 //}
 
 // Add event listener to generate button
-document.getElementById("generate").addEventListener("click", writePassword());
+document.getElementById("generate").addEventListener("click", writePassword);
